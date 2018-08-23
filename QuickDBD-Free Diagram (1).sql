0ff65a8279d87542a65cb0a1c098d79461bda3c3
Stephany Sanchez <stephanysanchez@Stephanys-MacBook-Air.local>
2018-08-23 14:54:54 -0400

    hello

---
+++ QuickDBD-Free Diagram (1).sql
@@ -0,0 +1,138 @@
+﻿-- Exported from QuickDBD: https://www.quickdatatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/schema/--1Xk15mf02EMCe_JpNgMg
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+CREATE TABLE `Customer` (
+    `CustomerID` id  NOT NULL ,
+    `firstName` string  NOT NULL ,
+    `lastname` string  NOT NULL ,
+    `email` string  NOT NULL ,
+    `password` string  NOT NULL ,
+    `phone` int  NOT NULL ,
+    `street` string  NOT NULL ,
+    `streetdetails` string  NOT NULL ,
+    `city` string  NOT NULL ,
+    `state` string  NOT NULL ,
+    `zip` int  NOT NULL ,
+    `hasdoorman` bolean  NOT NULL ,
+    `drycleaning` boolean  NOT NULL ,
+    `washandfold` boolean  NOT NULL ,
+    `orderid` id  NOT NULL ,
+    PRIMARY KEY (
+        `CustomerID`,`city`,`orderid`
+    )
+);
+
+CREATE TABLE `order` (
+    `orderid` id  NOT NULL ,
+    `pickupinstruction` string  NOT NULL ,
+    `dropoffinstruction` string  NOT NULL ,
+    `attend_pickup` boolean  NOT NULL ,
+    `atten_dropoff` boolean  NOT NULL ,
+    `pickupdateid` id  NOT NULL ,
+    `paymentID` id  NOT NULL ,
+    PRIMARY KEY (
+        `pickupdateid`,`paymentID`
+    )
+);
+
+CREATE TABLE `pickupdate` (
+    `pickupdateid` id  NOT NULL ,
+    `pick_upID` id  NOT NULL ,
+    `drop_offID` id  NOT NULL ,
+    PRIMARY KEY (
+        `pick_upID`,`drop_offID`
+    )
+);
+
+CREATE TABLE `pickup` (
+    `pickupid` id  NOT NULL ,
+    `Date` DATE  NOT NULL ,
+    `time` String  NOT NULL 
+);
+
+CREATE TABLE `dropoff` (
+    `dropoffid` id  NOT NULL ,
+    `Date` DATE  NOT NULL ,
+    `time` String  NOT NULL 
+);
+
+CREATE TABLE `status` (
+    `orderid` id  NOT NULL ,
+    `pick_up` boolean  NOT NULL ,
+    `drop_off` boolean  NOT NULL 
+);
+
+CREATE TABLE `payment` (
+    `paymentID` id  NOT NULL ,
+    `payment_date` string  NOT NULL ,
+    `payment` amount  NOT NULL ,
+    `payment_cardnumber` int  NOT NULL 
+);
+
+CREATE TABLE `driverlocation` (
+    `driverid` id  NOT NULL ,
+    `city` id  NOT NULL 
+);
+
+CREATE TABLE `drivers` (
+    `driverid` id  NOT NULL ,
+    `name` string  NOT NULL ,
+    `vehicle` string  NOT NULL ,
+    `time` string  NOT NULL ,
+    PRIMARY KEY (
+        `driverid`
+    )
+);
+
+CREATE TABLE `zipcode` (
+    `code` id  NOT NULL ,
+    `city` string  NOT NULL ,
+    PRIMARY KEY (
+        `code`
+    )
+);
+
+ALTER TABLE `order` ADD CONSTRAINT `fk_order_orderid` FOREIGN KEY(`orderid`)
+REFERENCES `Customer` (`orderid`);
+
+ALTER TABLE `order` ADD CONSTRAINT `fk_order_pickupdateid` FOREIGN KEY(`pickupdateid`)
+REFERENCES `pickupdate` (`pickupdateid`);
+
+ALTER TABLE `pickup` ADD CONSTRAINT `fk_pickup_pickupid` FOREIGN KEY(`pickupid`)
+REFERENCES `pickupdate` (`pick_upID`);
+
+ALTER TABLE `dropoff` ADD CONSTRAINT `fk_dropoff_dropoffid` FOREIGN KEY(`dropoffid`)
+REFERENCES `pickupdate` (`drop_offID`);
+
+ALTER TABLE `status` ADD CONSTRAINT `fk_status_orderid` FOREIGN KEY(`orderid`)
+REFERENCES `Customer` (`orderid`);
+
+ALTER TABLE `payment` ADD CONSTRAINT `fk_payment_paymentID` FOREIGN KEY(`paymentID`)
+REFERENCES `order` (`paymentID`);
+
+ALTER TABLE `drivers` ADD CONSTRAINT `fk_drivers_driverid` FOREIGN KEY(`driverid`)
+REFERENCES `driverlocation` (`driverid`);
+
+ALTER TABLE `zipcode` ADD CONSTRAINT `fk_zipcode_code` FOREIGN KEY(`code`)
+REFERENCES `Customer` (`zip`);
+
+ALTER TABLE `zipcode` ADD CONSTRAINT `fk_zipcode_city` FOREIGN KEY(`city`)
+REFERENCES `driverlocation` (`city`);
+
+CREATE INDEX `idx_Customer_firstName`
+ON `Customer` (`firstName`);
+
+CREATE INDEX `idx_Customer_lastname`
+ON `Customer` (`lastname`);
+
+CREATE INDEX `idx_Customer_email`
+ON `Customer` (`email`);
+
+CREATE INDEX `idx_Customer_password`
+ON `Customer` (`password`);
+
---
+++ QuickDBD-Free Diagram.sql
@@ -0,0 +1,135 @@
+﻿-- Exported from QuickDBD: https://www.quickdatatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/schema/--1Xk15mf02EMCe_JpNgMg
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+CREATE TABLE `Customer` (
+    `CustomerID` int  NOT NULL ,
+    `firstName` string  NOT NULL ,
+    `lastname` string  NOT NULL ,
+    `email` string  NOT NULL ,
+    `password` string  NOT NULL ,
+    `phone` int  NOT NULL ,
+    `street` string  NOT NULL ,
+    `streetdetails` string  NOT NULL ,
+    `city` string  NOT NULL ,
+    `state` string  NOT NULL ,
+    `zip` int  NOT NULL ,
+    `hasdoorman` bolean  NOT NULL ,
+    `drycleaning` boolean  NOT NULL ,
+    `washandfold` boolean  NOT NULL ,
+    `orderid` id  NOT NULL ,
+    PRIMARY KEY (
+        `CustomerID`,`orderid`
+    )
+);
+
+CREATE TABLE `laundrycarepackage` (
+    `laundrycarepackageID` id  NOT NULL ,
+    `name` string  NOT NULL ,
+    `price` double  NOT NULL ,
+    PRIMARY KEY (
+        `laundrycarepackageID`
+    )
+);
+
+CREATE TABLE `choose_service` (
+    `servicesID` id  NOT NULL ,
+    `laundrycarepackageID` id  NULL ,
+    `customwashdetail_ID` id  NULL ,
+    `drycleaningID` id  NULL ,
+    PRIMARY KEY (
+        `servicesID`,`drycleaningID`
+    )
+);
+
+CREATE TABLE `drycleaning` (
+    `drycleaningID` id  NOT NULL ,
+    `user_pick_drycleaning` boolean  NOT NULL ,
+    PRIMARY KEY (
+        `drycleaningID`
+    )
+);
+
+CREATE TABLE `item` (
+    `itemID` id  NOT NULL ,
+    `name` string  NOT NULL ,
+    `price` double  NOT NULL ,
+    PRIMARY KEY (
+        `itemID`
+    )
+);
+
+CREATE TABLE `dryitem` (
+    `drycleaningID` id  NOT NULL ,
+    `itemID` id  NOT NULL 
+);
+
+CREATE TABLE `customwashdetails` (
+    `customwashdetail_ID` id  NOT NULL ,
+    `whitetemperature` string  NOT NULL ,
+    `colortemprature` string  NOT NULL ,
+    `colorheat` string  NOT NULL ,
+    `whiteheat` string  NOT NULL ,
+    `soapID` id  NOT NULL ,
+    `softnerID` id  NOT NULL ,
+    `order` id  NOT NULL ,
+    PRIMARY KEY (
+        `customwashdetail_ID`
+    )
+);
+
+CREATE TABLE `soap` (
+    `soapID` id  NOT NULL ,
+    `name` string  NOT NULL ,
+    `price` double  NOT NULL ,
+    PRIMARY KEY (
+        `soapID`
+    )
+);
+
+CREATE TABLE `softner` (
+    `softnerID` id  NOT NULL ,
+    `name` string  NOT NULL ,
+    `price` double  NOT NULL ,
+    PRIMARY KEY (
+        `softnerID`
+    )
+);
+
+ALTER TABLE `Customer` ADD CONSTRAINT `fk_Customer_CustomerID` FOREIGN KEY(`CustomerID`)
+REFERENCES `choose_service` (`servicesID`);
+
+ALTER TABLE `choose_service` ADD CONSTRAINT `fk_choose_service_laundrycarepackageID` FOREIGN KEY(`laundrycarepackageID`)
+REFERENCES `laundrycarepackage` (`laundrycarepackageID`);
+
+ALTER TABLE `choose_service` ADD CONSTRAINT `fk_choose_service_customwashdetail_ID` FOREIGN KEY(`customwashdetail_ID`)
+REFERENCES `customwashdetails` (`customwashdetail_ID`);
+
+ALTER TABLE `choose_service` ADD CONSTRAINT `fk_choose_service_drycleaningID` FOREIGN KEY(`drycleaningID`)
+REFERENCES `dryitem` (`drycleaningID`);
+
+ALTER TABLE `dryitem` ADD CONSTRAINT `fk_dryitem_itemID` FOREIGN KEY(`itemID`)
+REFERENCES `item` (`itemID`);
+
+ALTER TABLE `soap` ADD CONSTRAINT `fk_soap_soapID` FOREIGN KEY(`soapID`)
+REFERENCES `customwashdetails` (`soapID`);
+
+ALTER TABLE `softner` ADD CONSTRAINT `fk_softner_softnerID` FOREIGN KEY(`softnerID`)
+REFERENCES `customwashdetails` (`softnerID`);
+
+CREATE INDEX `idx_Customer_firstName`
+ON `Customer` (`firstName`);
+
+CREATE INDEX `idx_Customer_lastname`
+ON `Customer` (`lastname`);
+
+CREATE INDEX `idx_Customer_email`
+ON `Customer` (`email`);
+
+CREATE INDEX `idx_Customer_password`
+ON `Customer` (`password`);
+
